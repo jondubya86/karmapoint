a0235ca7d64e185366c49e6151d74176a86324bf
bryan <bryanbethea28@gmail.com>
2017-01-21 17:31:09 -0500

    userpage

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,12 +1,20 @@
 // import {withRouter, Router, Route, Link, browserHistory} from 'react-router';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import Mainpage from './mainpage/mainpage'
+import Userform from './mainpage/userform'
 
 
 const App = React.createClass({
 	render(){
 		return(
-			<div>Hello from app.js</div>
+			
+      <div>
+      <Mainpage />
+        <div style = {{height: 50}}>
+        </div>
+      <Userform />
+      </div>
 		)
 	}
 })
---
+++ frontend/src/mainpage/interest.jsx
@@ -0,0 +1,18 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+
+const Interest = React.createClass({
+  render(){
+    return(
+      
+        <div style = {{display: "flex",justifyContent: "center"}}>
+
+         
+        </div>
+
+    )
+  }
+})
+
+export default Interest

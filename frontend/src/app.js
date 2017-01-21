f30ec24477985aaa66f03ca63cfb9a930b45efc4
Carlos Martinez <martinezcj2@gmail.com>
2017-01-21 18:55:03 -0500

    created react router

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,6 +1,9 @@
 // import {withRouter, Router, Route, Link, browserHistory} from 'react-router';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import {Router, Route, browserHistory, IndexRoute} from 'react-router';
+
+// Components
 import Mainpage from './mainpage/mainpage'
 import Userform from './mainpage/userform'
 
@@ -8,7 +11,7 @@ import Userform from './mainpage/userform'
 const App = React.createClass({
 	render(){
 		return(
-			
+
       <div>
       <Mainpage />
         <div style = {{height: 50}}>
@@ -20,6 +23,11 @@ const App = React.createClass({
 })
 
 ReactDOM.render(
-	<App />,
+	<Router history={browserHistory}>
+		<Route path="/" component={App}>
+			{/* <IndexRoute /> */}
+			<Route path="/result" component={ResultPage} />
+		</Route>
+	</Router>,
   document.getElementById('root')
 )

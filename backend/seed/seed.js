44232abeb78c6da41d84d17b38f884a5ebc84fc4
Jung Chen <jungwchen@gmail.com>
2017-01-21 19:44:37 -0500

    updated seed for models

--- backend/seed/seed.js
+++ backend/seed/seed.js
@@ -4,10 +4,10 @@ const Comment = require('../models').Comment;
 const db = require('../models').sequelize;
 
 let users = [
-  {name: 'Bryan', zipcode: 11210, karmapoints: 500},
-  {name: 'Carlos', zipcode: 11016, karmapoints: 1000},
-  {name: 'Jon', zipcode: 10002, karmapoints: 400},
-  {name: 'Jung', zipcode: 11102, karmapoints: 400}
+  {name: 'Bryan', zipcode: 11210, karmapoints: 500, bio: 'I joined Access Code to further my knowledge in software development. I hope to first acquire the skill set to land a job in the tech industry then ultimately pursue my own entrepreneurial endeavors. One fun fact about me is that on some nights when I fall asleep alone, I wake up with a 60lb pitbull snoring on my chest. Another fun fact is that I am originally from North Carolina and I moved to NYC to pursue music.'},
+  {name: 'Carlos', zipcode: 11016, karmapoints: 1000, bio: 'My name is Carlos Martinez. I live in Bed-Stuy Brooklyn. I joined Access Code because I believe it is an amazing opportunity to get into the tech world. My goal is to obtain a job with a tech company. Whether is is for a small start-up or a large tech company. I tend to read a lot of non-fiction books as I think it is a great way to learn information and lessons from different people. I also love talking about the Universe! You like Carl Sagan? Let’s chat it up.'},
+  {name: 'Jon', zipcode: 10002, karmapoints: 400, bio:"I have been searching for a program to learn more about programming in general and came across C4Q. I love the pay it forward structure that encourages learning first unlike most other programs I’ve come across. I applied in hopes of learning web development and by the end have an up and running retail webpage. Two fun facts are I am an amateur photographer and I’m also a recovering sneakerhead."},
+  {name: 'Jung', zipcode: 11102, karmapoints: 400, bio:"I joined access code because I wanted a change of pace from what I was studying, which was biochemistry. I felt that I can do more with programming than through biochemistry where I will have to go through multiple years of academia in order to get to where I want to be. My hobbies right now are reading, playing video games, and hanging out with friends."}
 ]
 
 let comments = [

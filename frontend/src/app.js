c78a69af9939cc2e73124938205538e9b861c851
Jung Chen <jungwchen@gmail.com>
2017-01-22 18:44:42 -0500

    removed google api key

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2,14 +2,11 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import {Router, Route, browserHistory, Link,IndexRoute} from 'react-router';
-// import ResultItem from './results/res7'
+
 // Components
 import Mainpage from './mainpage/mainpage';
-
 import MainResult from './results/resultItem';
-
 import ProgramInfo from './message/programinfo';
-
 import Userform from './mainpage/userform';
 import UserAccountPage from './components/userAccount/UserAccountPage';
 import Footer from './components/Footer';
@@ -33,19 +30,10 @@ ReactDOM.render(
 	<Router history={browserHistory}>
 		<Route path="/" component={App}>
 			<IndexRoute component={Mainpage}/>
-
-
-			
-
-			{/* <Route path="/result" component={ResultPage} /> */}
-
-			{/*<Route path="/result" component={App} />*/}
-			{/*change path of companyinfo to :programId */}
+			<Route path="/result" component={MainResult} /> 
 			<Route path='/program/:programId' component={ProgramInfo} />
-
 			<Route path="/account" component={UserAccountPage} />
 		</Route>
-			<Route path="/result" component={MainResult} /> 
 	</Router>,
   document.getElementById('root')
 )
